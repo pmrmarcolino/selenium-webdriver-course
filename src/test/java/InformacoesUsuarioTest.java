46a0f9675fc0571415dfe9312bab208fe1d48276
pmrmarcolino <pmrmarcolino@hotmail.com>
2018-11-26 15:46:50 -0200

    Identificando Elementos

--- src/test/java/InformacoesUsuarioTest.java
+++ src/test/java/InformacoesUsuarioTest.java
@@ -1,17 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTest {
 
     @Test
     public void testAdicionarUmaInfoAdicionalUsuario(){
-        System.setProperty("webdriver.chrome.driver","/home/usertqi/ambiente/Drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/home/usertqi/Ambiente/udemy/drivers/chromedriver");
         WebDriver navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         navegador.get("http://www.juliodelima.com.br/taskit");
+        navegador.findElement(By.linkText("Sign in")).click();
+        navegador.findElement(By.id("signinbox")).findElement(By.name("login")).sendKeys("julio0001");
+        navegador.findElement(By.id("signinbox")).findElement(By.name("password")).sendKeys("123456");
+        navegador.findElement(By.linkText("SIGN IN")).click();
+        String login = navegador.findElement(By.className("me")).getText();
 
-        assertEquals(1,1);
+        assertEquals("Hi, Julio",login);
     }
 }
