3573fe630b7dfabec5ec16e0895ef88f42ecd8e0
patricia <pmrmarcolino@hotmail.com>
2019-01-03 17:04:38 -0200

    Teste em nuvem

--- src/test/java/suporte/Web.java
+++ src/test/java/suporte/Web.java
@@ -2,10 +2,19 @@ package suporte;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Web {
+
+    public static final String USERNAME = "";
+    public static final String AUTOMATE_KEY = "";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
     public static WebDriver createChrome(){
 
         System.setProperty("webdriver.chrome.driver","/home/usertqi/drivers/chromedriver");
@@ -15,4 +24,27 @@ public class Web {
         navegador.get("http://www.juliodelima.com.br/taskit");
         return navegador;
     }
+
+    public static WebDriver createBrowserStack(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "60.0");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1024x768");
+
+        WebDriver driver = null;
+
+        try {
+            driver = new RemoteWebDriver(new URL(URL), caps);
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+            driver.get("http://www.juliodelima.com.br/taskit");
+        } catch (MalformedURLException e){
+            e.getStackTrace();
+        }
+
+        return driver;
+    }
+
 }
